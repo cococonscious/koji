1fb6aca994a79004e0f84649490ce4ca209c7c1a
Danny Tatom <its.danny@hey.com>
2021-11-03 15:19:17 -0700

    feat: allow passing path to a config file
    closes #23

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -4,17 +4,17 @@ use std::{fs, path::Path};
 
 use crate::commit_types::CommitType;
 
-const CONFIG_FILE: &str = "koji.toml";
-
 #[derive(Clone, Deserialize)]
 pub struct Config {
     pub commit_types: Vec<CommitType>,
 }
 
 /// Loads `koji.toml` if there is one, otherwise use the default.
-pub fn load_config() -> Result<Config> {
-    if Path::new(CONFIG_FILE).exists() {
-        let file = fs::read_to_string(CONFIG_FILE).context("could not read config file")?;
+pub fn load_config(path: Option<&str>) -> Result<Config> {
+    let path = path.unwrap_or("koji.toml");
+
+    if Path::new(path).exists() {
+        let file = fs::read_to_string(path).context("could not read config file")?;
         let parsed: Config =
             toml::from_str(file.as_ref()).context("could not parse config file")?;
 
@@ -24,5 +24,26 @@ pub fn load_config() -> Result<Config> {
         let parsed: Config = toml::from_str(file).context("could not parse default config file")?;
 
         Ok(parsed)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_config_default() {
+        let blah = load_config(None).unwrap();
+        let first = blah.commit_types.get(0).unwrap();
+
+        assert_eq!(first.description, "A new feature");
+    }
+
+    #[test]
+    fn test_load_config_with_arg() {
+        let blah = load_config(Some("./meta/config/koji-no-emoji.toml")).unwrap();
+        let first = blah.commit_types.get(0).unwrap();
+
+        assert_eq!(first.emoji, None);
     }
 }
