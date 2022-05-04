d6f91d70fb75ffcc118427d2fad181c6f8fd8571
Danny Tatom <its.danny@hey.com>
2022-05-04 14:06:44 -0700

    refactor: move commit code to its own file

---
+++ src/lib/commit.rs
@@ -0,0 +1,46 @@
+use std::{fs::File, io::Write};
+
+use anyhow::Result;
+use cocogitto::CocoGitto;
+use git2::Repository;
+
+/// Output a commit message to `.git/COMMIT_EDITMSG`
+pub fn write_commit_msg(
+    repo: Repository,
+    commit_type: String,
+    scope: Option<String>,
+    summary: String,
+    body: Option<String>,
+    is_breaking_change: bool,
+) -> Result<()> {
+    let message = CocoGitto::get_conventional_message(
+        &commit_type,
+        scope,
+        summary,
+        body,
+        None,
+        is_breaking_change,
+    )?;
+
+    let commit_editmsg = repo.path().join("COMMIT_EDITMSG");
+    let mut file = File::create(commit_editmsg)?;
+
+    file.write_all(message.as_bytes())?;
+
+    Ok(())
+}
+
+/// Create a commit
+pub fn commit(
+    commit_type: String,
+    scope: Option<String>,
+    summary: String,
+    body: Option<String>,
+    is_breaking_change: bool,
+) -> Result<()> {
+    let cocogitto = CocoGitto::get()?;
+
+    cocogitto.conventional_commit(&commit_type, scope, summary, body, None, is_breaking_change)?;
+
+    Ok(())
+}
